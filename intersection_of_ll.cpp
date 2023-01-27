e638c6311750f0616bcc3e702621bf735a56b73c
Kartikey Solanki <kartikey.rajput0071@gmail.com>
2023-01-27 15:23:13 +0530

    only check intersectionOfTwoLinkedLists() function

---
+++ intersection_of_ll.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+// This function gets two arguments - the head pointers of the two linked lists
+// Return the node which is the intersection point of these linked lists
+// It is assured that the two lists intersect
+Node *intersectionOfTwoLinkedLists(Node *l1, Node *l2)
+{
+    /*Code here*/
+    Node *head1 = l1 ;
+    Node *head2 = l2 ;
+    
+    int len1 = 0, len2 = 0, diff ;
+    
+    // calculating the length of first linked list
+    while(head1 != NULL)
+    {
+        head1 = head1 -> next ;
+        len1++ ;
+    }
+
+    // calculating the length of second linked list
+    while (head2 != NULL)
+    {
+        head2 = head2 -> next ;
+        len2++ ;
+    }
+
+    // checking if the first linked list is larger
+    if (len1 > len2)
+    {
+        diff = len1 - len2 ;
+        while (diff > 0)
+        {
+            l1 = l1 -> next ; // changing the value of head of first linked list
+            diff-- ;
+        }
+    }
+
+    if (len2 > len1)
+    {
+        diff = len2 - len1 ;
+        while (diff > 0)
+        {
+            l2 = l2 -> next ;
+            diff-- ;
+        }
+    }
+
+    // comparing the lists for point of intersection
+    while (l1 != NULL and l2 != NULL)
+    {
+        if (l1 == l2)
+        {
+            return l1 ;
+        }
+        l1 = l1 -> next ;
+        l2 = l2 -> next ;
+    }
+    return NULL ;
+}
+
+
+
+/*
+*
+*
+*   You do not need to refer or modify any code below this. 
+*   Only modify the above function definition.
+*	Any modications to code below could lead to a 'Wrong Answer' verdict despite above code being correct.
+*	You do not even need to read or know about the code below.
+*
+*
+*
+*/
+
+Node *buildList(unordered_map<int, Node *> &hash)
+{
+    int x;
+    cin >> x;
+    Node *head = new Node(x);
+    Node *current = head;
+    hash[x] = head;
+    while (x != -1)
+    {
+        cin >> x;
+        if (x == -1)
+            break;
+        Node *n = new Node(x);
+        hash[x] = n;
+        current->next = n;
+        current = n;
+    }
+    current->next = NULL;
+    return head;
+}
+
+void printLinkedList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    unordered_map<int, Node *> hash;
+    Node *l1 = buildList(hash);
+
+    Node *l2 = NULL;
+    int x;
+    cin >> x;
+    l2 = new Node(x);
+    Node *temp = l2;
+
+    while (x != -1)
+    {
+        cin >> x;
+        if (x == -1)
+            break;
+        if (hash.find(x) != hash.end())
+        {
+            temp->next = hash[x];
+            break;
+        }
+        Node *n = new Node(x);
+        temp->next = n;
+        temp = n;
+    }
+
+    cout << "L1 - ";
+    printLinkedList(l1);
+    cout << "L2 - ";
+    printLinkedList(l2);
+
+    Node *intersectionPoint = intersectionOfTwoLinkedLists(l1, l2);
+    cout << "Intersection at node with data = " << intersectionPoint->data << endl;
+
+    return 0;
+}
